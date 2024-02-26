8c5b1d1a6f0e75e501fff5ab38a64ee947ffd5aa
lstsystems <leyter@leytersuarez.com>
2024-02-26 16:41:32 -0500

    Changed the inheritance to Exception

--- Cohors/Exceptions/NotFoundException.cs
+++ Cohors/Exceptions/NotFoundException.cs
@@ -2,5 +2,12 @@ using System.Net;
 
 namespace Cohors.Exceptions;
 
-public class NotFoundException<T>(int id)
-    : HttpException($"{typeof(T)} with id of {id} not found!", HttpStatusCode.NotFound);
+/// <summary>
+///   Represents an exception related to an entity not being found.
+/// </summary>
+/// <param name="entityId"></param>
+/// <typeparam name="T"></typeparam>
+public class NotFoundException<T>(int entityId) : Exception($"{typeof(T)} with id of {entityId} not found!")
+{
+    public HttpStatusCode StatusCode { get; } = HttpStatusCode.NotFound;
+}
