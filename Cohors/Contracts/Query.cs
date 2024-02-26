708ea7555f0fd497e4df8ec17cd37b25527b880e
lstsystems <leyter@leytersuarez.com>
2024-02-26 13:21:55 -0500

    Added Comments

--- Cohors/Contracts/Query.cs
+++ Cohors/Contracts/Query.cs
@@ -6,13 +6,29 @@ public sealed class Query<T> where T : class
 {
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public T? Data { get; set; } = default(T);
+    
+    // The PageSize specifies the number of items to display per page.
     public int PageSize { get; set; } = 10;
+    // The PageNumber specifies the current page number.
     public int PageNumber { get; set; } = 1;
-    public string SortBy { get; set; } = "Id";
-    public string SortDirection { get; set; } = "asc";
-    public string? SearchTerm { get; set; }
-    public string? SearchBy { get; set; }
-    public string? FilterBy { get; set; }
-    public string? FilterValue { get; set; }
+    // The TotalPages specifies the total number of pages in the database.
     public int  TotalPages { get; set; } = 1;
+    
+    // The SortBy specifies the column to sort by.
+    public string SortBy { get; set; } = "Id";
+    // The SortDirection specifies the direction to sort by. Usually asc or desc
+    public string SortDirection { get; set; } = "asc";
+    
+    
+    // The SearchTerm specifies the string to search for in the database.
+    public string? SearchTerm { get; set; }
+    // The SearchBy specifies the column to search for the SearchTerm. Usually the name or title column
+    public string? SearchBy { get; set; }
+    
+    
+    // The FilterBy specifies the column to filter by.
+    public string? FilterBy { get; set; }
+    // The FilterValue specifies the value to filter by.
+    public string? FilterValue { get; set; }
+    
 }
